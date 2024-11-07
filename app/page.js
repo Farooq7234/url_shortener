de084a37b20d366d8972f789ec15aa62ebcfad7b
Umar Farooq <ufservices465@gmail.com>
2024-11-07 11:36:59 +0530

    last commit

--- app/page.js
+++ app/page.js
@@ -1,21 +1,15 @@
 import Image from "next/image";
-import localFont from "next/font/local";
 import Link from "next/link";
-import Spline from "@splinetool/react-spline";
-
-
-
-
 
 export default function Home() {
   return (
-    <main className="bg-black flex justify-around w-full items-center text-white min-h-screen">
-      <section className="grid grid-cols-2 w-full px-10">
-        <div className="flex flex-col gap-4 items-center justify-center ">
-          <h2 className={`text-3xl  font-bold`}>
+    <main className="bg-black text-white min-h-screen flex justify-center items-center">
+      <section className="grid grid-cols-2 h-[50vh]">
+        <div className="flex flex-col gap-4 items-center justify-center">
+          <p className={`text-3xl font-bold`}>
             The best URL shortener in the Market
-          </h2>
-          <p className=" text-center w-full">
+          </p>
+          <p className="px-10 text-center">
             We are the most straightfoward URL Shortener in the world. Most of the url shorteners will track you or ask you to give your details for login. We understand your needs and hence we have created this URL shortener
           </p>
           <div className='flex gap-3 justify-start'>
@@ -23,10 +17,10 @@ export default function Home() {
           <Link href="/github"><button className='bg-purple-500 rounded-lg shadow-lg p-3 py-1 font-bold text-white'>GitHub</button></Link>
         </div>
         </div>
-        
-        <Spline
-        scene="https://prod.spline.design/6Fc61-mHiANRL00J/scene.splinecode" 
-      />
+        <div className=" flex justify-start relative">
+          <Image className="" alt="an Image of a vector" src={"/vector.jpg"} fill={true}    />
+        </div>
+
       </section>
     </main>
   );
